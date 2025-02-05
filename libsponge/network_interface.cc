194d059e4435f7612560d9635286d04b6f810bd2
cfz <cfz11330216@163.com>
2025-02-05 16:05:02 +0800

    完成lab5

--- libsponge/network_interface.cc
+++ libsponge/network_interface.cc
@@ -32,15 +32,80 @@ NetworkInterface::NetworkInterface(const EthernetAddress &ethernet_address, cons
 void NetworkInterface::send_datagram(const InternetDatagram &dgram, const Address &next_hop) {
     // convert IP address of next hop to raw 32-bit representation (used in ARP header)
     const uint32_t next_hop_ip = next_hop.ipv4_numeric();
-
-    DUMMY_CODE(dgram, next_hop, next_hop_ip);
+    EthernetFrame frame;
+    frame.header().type = EthernetHeader::TYPE_IPv4;
+    frame.header().src = _ethernet_address;
+    frame.payload() = move(dgram.serialize());
+    if (_arp_map.count(next_hop_ip) && _arp_map[next_hop_ip].second > _time) {
+        frame.header().dst = _arp_map[next_hop_ip].first;
+        _frames_out.emplace(move(frame));
+    } else {
+        if (!_arps_out.count(next_hop_ip) || _arps_out[next_hop_ip] <= _time) {
+            ARPMessage request_arp;
+            request_arp.opcode = ARPMessage::OPCODE_REQUEST;
+            request_arp.sender_ethernet_address = _ethernet_address;
+            request_arp.sender_ip_address = _ip_address.ipv4_numeric();
+            request_arp.target_ip_address = next_hop_ip;
+            _arps_out[next_hop_ip] = _time + 5000;
+            EthernetFrame request_frame;
+            request_frame.header().type = EthernetHeader::TYPE_ARP;
+            request_frame.header().src = _ethernet_address;
+            request_frame.header().dst = ETHERNET_BROADCAST;
+            request_frame.payload() = move(request_arp.serialize());
+            _frames_out.emplace(move(request_frame));
+        }
+        _frames_waiting.emplace(next_hop_ip, move(frame));
+    }
 }
 
 //! \param[in] frame the incoming Ethernet frame
 optional<InternetDatagram> NetworkInterface::recv_frame(const EthernetFrame &frame) {
-    DUMMY_CODE(frame);
-    return {};
+    if (frame.header().dst != ETHERNET_BROADCAST && frame.header().dst != _ethernet_address) {
+        return nullopt;
+    }
+    if (frame.header().type == EthernetHeader::TYPE_IPv4) {
+        InternetDatagram datagram;
+        if (datagram.parse(frame.payload()) == ParseResult::NoError) {
+            return datagram;
+        } else {
+            return nullopt;
+        }
+    } else if (frame.header().type == EthernetHeader::TYPE_ARP) {
+        ARPMessage msg;
+        if (msg.parse(frame.payload()) == ParseResult::NoError) {
+            _arp_map[msg.sender_ip_address] = {msg.sender_ethernet_address, _time + 30000};
+            if (msg.opcode == ARPMessage::OPCODE_REQUEST && msg.target_ip_address == _ip_address.ipv4_numeric()) {
+                ARPMessage reply_arp;
+                reply_arp.opcode = ARPMessage::OPCODE_REPLY;
+                reply_arp.sender_ethernet_address = _ethernet_address;
+                reply_arp.sender_ip_address = _ip_address.ipv4_numeric();
+                reply_arp.target_ethernet_address = msg.sender_ethernet_address;
+                reply_arp.target_ip_address = msg.sender_ip_address;
+                EthernetFrame reply_frame;
+                reply_frame.header().type = EthernetHeader::TYPE_ARP;
+                reply_frame.header().src = _ethernet_address;
+                reply_frame.header().dst = msg.sender_ethernet_address;
+                reply_frame.payload() = move(reply_arp.serialize());
+                _frames_out.emplace(move(reply_frame));
+            }
+            while (!_frames_waiting.empty()) {
+                EthernetFrame waiting_frame = _frames_waiting.front().second;
+                if (_arp_map.count(_frames_waiting.front().first) && _arp_map[_frames_waiting.front().first].second > _time) {
+                    waiting_frame.header().dst = _arp_map[_frames_waiting.front().first].first;
+                    _frames_waiting.pop();
+                    _frames_out.emplace(move(waiting_frame));
+                } else {
+                    break;
+                }
+            }
+        } else {
+            return nullopt;
+        }
+    }
+    return nullopt;
 }
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
-void NetworkInterface::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); }
+void NetworkInterface::tick(const size_t ms_since_last_tick) {
+    _time += ms_since_last_tick;
+}
--- libsponge/network_interface.hh
+++ libsponge/network_interface.hh
@@ -7,6 +7,8 @@
 
 #include <optional>
 #include <queue>
+#include <map>
+#include <utility>
 
 //! \brief A "network interface" that connects IP (the internet layer, or network layer)
 //! with Ethernet (the network access layer, or link layer).
@@ -39,6 +41,11 @@ class NetworkInterface {
 
     //! outbound queue of Ethernet frames that the NetworkInterface wants sent
     std::queue<EthernetFrame> _frames_out{};
+
+    std::unordered_map<uint32_t, std::pair<EthernetAddress, size_t>> _arp_map{};
+    std::queue<std::pair<uint32_t, EthernetFrame>> _frames_waiting{};
+    std::unordered_map<uint32_t, size_t> _arps_out{};
+    size_t _time = 0;
 
   public:
     //! \brief Construct a network interface with given Ethernet (network-access-layer) and IP (internet-layer) addresses
